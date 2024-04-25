88a4f230c989703166d3cc60defa2f5d22e9e067
tairasoul <100938045+tairasoul@users.noreply.github.com>
2024-04-25 14:28:34 +0200

    forgot to include settingsapi by default, woops

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ cd bin/Release/net48
 cp Newtonsoft.Json.dll ../../../SpeedrunningUtils
 cp SpeedrunningUtils.dll ../../../SpeedrunningUtils
 cp TcpSharp.dll ../../../SpeedrunningUtils
+cp SettingsAPI.dll ../../../SpeedrunningUtils
 cd ..
 cd ..
 cd ..
